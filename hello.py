f58b2db1795004d02b7d32e2a911a855072ee7aa
irenesengo <irenesengo@gmail.com>
2023-11-22 09:55:52 +0000

    updates

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 @app.route("/")#URL leading to method
 
 def hello(): # Name of the method
- return("Hello World!") #indent this line
+ return("Hello Murat!!") #indent this line
 if __name__ == "__main__":
  app.run(host='0.0.0.0', port='8080') # indent this line
 
